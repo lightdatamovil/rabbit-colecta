dd00d77587c7a55d3ca972180ce1362658881ff7
Gonn01 <gonzalomiguelrigoni@gmail.com>
2025-03-11 12:15:20 -0300

    Refactor checkearEstadoEnvio function to remove unnecessary database connection closures; update version to 2.0.1

--- controller/colectaController/functions/checkarEstadoEnvio.js
+++ controller/colectaController/functions/checkarEstadoEnvio.js
@@ -7,11 +7,9 @@ export async function checkearEstadoEnvio(dbConnection, shipmentId) {
 
     if (estado.length > 0) {
         if (estado[0].estado_envio == 5 || estado[0].estado_envio == 9 || estado[0].estado_envio == 8) {
-            dbConnection.end();
             return { estadoRespuesta: false, mensaje: "El paquete ya fue entregado o cancelado" };
         }
         if (estado[0].estado_envio == 0) {
-            dbConnection.end();
             return { estadoRespuesta: false, mensaje: "El paquete ya se encuentra colectado" };
         }
     }
