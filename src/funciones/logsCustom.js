8bbaca27c01dd8cfa76d8ffcb3abf01c3556bdb5
Gonn01 <gonzalomiguelrigoni@gmail.com>
2025-03-12 10:13:06 -0300

    nd

--- src/funciones/logsCustom.js
+++ src/funciones/logsCustom.js
@@ -1,27 +1,27 @@
 import os from "os";
 
 const isLocal = ["localhost", "127.0.0.1"].includes(os.hostname());
-console.log(os.hostname());
+console.log(`asdasdasdasdsadasa ${os.hostname()}`);
 
 export function logGreen(message) {
     if (isLocal) {
-        console.log(`\x1b[32m%s\x1b[0m`, message);
+        console.log(`\x1b[32m%s\x1b[0m`, `✅ ${message}`);
     }
 }
 
 export function logRed(message) {
     if (isLocal) {
-        console.log(`\x1b[31m%s\x1b[0m`, message);
+        console.log(`\x1b[31m%s\x1b[0m`, `❌ ${message}`);
     }
 }
 
 export function logBlue(message) {
     if (isLocal) {
-        console.log(`\x1b[34m%s\x1b[0m`, message);
+        console.log(`\x1b[34m%s\x1b[0m`, `🔵 ${message}`);
     }
 }
 export function logYellow(message) {
     if (isLocal) {
-        console.log(`\x1b[33m%s\x1b[0m`, message);
+        console.log(`\x1b[33m%s\x1b[0m`, `⚠️ ${message}`);
     }
 }
